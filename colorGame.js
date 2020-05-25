a4993d096f9bb2bcc83f745997438effba10bc0b
khuunter@yahoo.com <khuunter@yahoo.com>
2020-05-25 16:15:43 -0400

    pick color function added

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,7 @@ let colors = [
 ]
 
 let squares =  document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message")
 
@@ -39,4 +39,10 @@ function changeColors(color){
     // change each color to match given color
     squares[i].style.backgroundColor = color;
   }
+}
+
+function pickColor(){
+  // randomly pick rgb color
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
